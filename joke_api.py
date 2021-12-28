80c90b0b05056ea75fe29666245ef2b3f7575500
shubhajeet <shubha.pradhan12@gmail.com>
2021-12-28 22:01:20 +0530

    lmbda function added

---
+++ joke_api.py
@@ -0,0 +1,17 @@
+import requests
+
+URL = 'https://official-joke-api.appspot.com/random_joke'
+
+
+def check_valid_status_code(request):
+    if request.status_code == 200:
+        return request.json()
+
+    return False
+
+
+def get_joke():
+    request = requests.get(URL)
+    data = check_valid_status_code(request)
+
+    return data
--- main.py
+++ main.py
@@ -1,30 +1,64 @@
 import discord
-import os
 import requests
 import json
 
-client = discord.Client()
 
-def get_news():
-  response = requests.get\
-  ("https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=adb3aa9959564110862634aa0b8ae6ba")
-  json_data = json.loads(response.text)
-  news = json_data[0]['content']
-  return(news)
+class myClient(discord.Client):
+    async def on_ready(self):
+        print(f'Howdy 👋 ! Logged in as {client.user}'.format(client))
+    async def on_message(self, ctx):
+        if ctx.author == client.user:
+            return
+        elif ctx.content.startswith('hello'):
+            await ctx.channel.send('Hello, my friend 👋')
+        elif ctx.content.startswith('make me laugh'):
+            await ctx.channel.send('Would you like to hear a cliche joke 😛 ?')
+            res = await client.wait_for('message', check=lambda message: ctx.author == message.author)
+            if res.content.lower() == ('yes'):
+                async def get_joke(self):
+                    response = requests.get(
+                        'https://api.chucknorris.io/jokes/random')
+                    joke = json.loads(response.text)
+                    # print(joke)
+                    return(joke['value'])
+                the_joke = await get_joke(self)
+                await ctx.channel.send(the_joke)
+            else:
+                await ctx.channel.send('no problem!')
+        elif ctx.content.startswith('tell me a joke'):
+            await ctx.channel.send('Would you like to hear a clihe joke 😛 ?')
+            res = await client.wait_for('message', check=lambda message: ctx.author == message.author)
+            if res.content.lower() == ('yes'):
+                async def get_joke(self):
+                    response = requests.get(
+                        'https://api.chucknorris.io/jokes/random')
+                    joke = json.loads(response.text)
+                    # print(joke)
+                    return(joke['value'])
+                the_joke = await get_joke(self)
+                await ctx.channel.send(the_joke)
+            else:
+                await ctx.channel.send('no problem!')
+                async def get_joke(self):
+                    response = requests.get(
+                        'https://api.chucknorris.io/jokes/random')
+                    joke = json.loads(response.text)
+                    # print(joke)
+                    return(joke['value'])
+                the_joke = await get_joke(self)
+                await ctx.channel.send(the_joke)
+        elif ctx.content.startswith('hii'):
+            await ctx.channel.send('Hello 👋')
+        elif ctx.content.startswith('yo'):
+            await ctx.channel.send('Yo Dude 👋')
+        elif ctx.content.startswith('who created you?'):
+            await ctx.channel.send('Shubhajeet Pradhan 😎')
+        elif ctx.content.startswith('do you know me?'):
+            await ctx.channel.send('Ya !! since you are in this channel then definately you will be his friend 😏')
+        elif ctx.content.startswith('bye'):
+            await ctx.channel.send('tata bye bye 👋')
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+client = myClient()
+# Bot login using the token
+client.run('OTI0OTQ4NTMwMTk4NjM0NTE2.Ycl_cg.BV-ZYj16yeghZbjxYMwuDyMdt70')
 
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$news'):
-      news = get_news()
-      await message.channel.send(news)
-
-
-client.run(os.getenv('TOKEN'))
